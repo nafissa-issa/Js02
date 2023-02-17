f14cbc24ae34755c3233eb44605662a2cd2ec97e
nafissa-issa <124690665+nafissa-issa@users.noreply.github.com>
2023-02-17 15:43:54 +0100

    Update myStr.js

--- myStr.js
+++ myStr.js
@@ -1,4 +1,4 @@
-let myStr1 = " this is the start ";
-let myStr2 = " this is the end ";
+let myStr1 = " This is the start ";
+let myStr2 = " This is the end ";
 let myStr = myStr1+ "" +myStr2 
 console.log(myStr);
